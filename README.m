4751893d5e34759ba090a36852ec07bba97f17a0
RaffsBr <144849390+RaffsBr@users.noreply.github.com>
2023-09-12 18:00:45 -0300

    Create README.md

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @RaffsBr
+- 👀 I’m interested in learn everything about software and computer science.
+- 🌱 I’m currently learning all technologies, languages ​​and their specifications.
+- 💞️ I'm looking to collaborate with any company that seeks to teach me everything I need to learn throughout my professional career.
+- 📫 How to reach me: my gmail is rafaelmunizr09@gmail.com
+
+<!---
+RaffsBr/RaffsBr is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
